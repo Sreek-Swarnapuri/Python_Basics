bf91302878d735de9bc353d27c702780c81dec43
Sreekar Swarnapuri Pandadu <sreekar.swarnapuri@hotmail.com>
2019-09-17 23:23:16 +0530

    Update ListOperations.py

--- ListOperations.py
+++ ListOperations.py
@@ -10,3 +10,10 @@ print(str*3)
 lst = [1,2,3,4,5]
 print(lst+[6,7,8])
 print(lst*3)
+
+#To check if an item is in a list, 'in' keyword can be used
+lst = [1,2,3,4,5,6,7,"Hello","World"]
+print(4 in lst)
+print(100 in lst)
+print("Hello" in lst)
+print("world" in lst)
