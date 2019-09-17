a5d41d007d3150009d13f8fc9675b98c7a25ce9c
Sreekar Swarnapuri Pandadu <sreekar.swarnapuri@hotmail.com>
2019-09-17 23:19:55 +0530

    Update and rename ListOperations to ListOperations.py

---
+++ ListOperations.py
@@ -0,0 +1,12 @@
+#An Item at a certain index can be re assigned.
+numbers = [7,7,7,7,7,7]
+numbers[3] = 2
+print(numbers)
+
+#Lists can be added and multiplied same way as strings
+str = "Hello"
+print(str+" suffixing")
+print(str*3)
+lst = [1,2,3,4,5]
+print(lst+[6,7,8])
+print(lst*3)
