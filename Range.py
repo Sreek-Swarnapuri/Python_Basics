a25c4fd8f11b8c70bbd22eaff8a4505abc0fa0ba
Sreekar Swarnapuri Pandadu <sreekar.swarnapuri@hotmail.com>
2019-09-19 22:42:30 +0530

    Update Range.py

--- Range.py
+++ Range.py
@@ -1,4 +1,8 @@
 #Range function creates sequential list of numbers
+#Range method itself creates a range object, we would need to call list fucntion in order to convert it to a list
 numbers = list(range(10))
 print(numbers)
+rng = range(5)
+print(type(rng))
+print(rng)
 
