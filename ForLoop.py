c6e6cfff187f66f93ad3ffb380e746ba4e37d0d0
Sreekar Swarnapuri Pandadu <sreekar.swarnapuri@hotmail.com>
2019-09-22 09:15:19 +0530

    Update ForLoop.py

--- ForLoop.py
+++ ForLoop.py
@@ -3,7 +3,7 @@ lst = [1,2,3,4,5,7]
 for i in lst:
   print(i)
 
-  #for loop with range funciton
+#for loop with range funciton
 #We can use for loop to iterate through a range
 for i in range(5):
-  print(i)
+  print("hello!")
