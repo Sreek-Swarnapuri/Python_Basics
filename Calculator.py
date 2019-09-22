2610193eea1501270e658f8a929d9ed26049e7fd
Sreekar Swarnapuri Pandadu <sreekar.swarnapuri@hotmail.com>
2019-09-22 20:55:24 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -3,19 +3,23 @@ while True:
   print("Enter '+' or '-' or '*' or '/' for any operations.")
   print("Enter 'quit' to end the program")
   user_input = input(": ")
-  print("Enter the first number")
-  a = input(": ")
-  print("Enter the second number")
-  b = input(": ")
   if user_input == 'quit':
     break
-  elif user_input == '+':
-    print("Additing of the two numbers is ", a+b)
+  print("Enter the first number")
+  a = int(input(": "))
+  print("Enter the second number")
+  b = int(input(": "))
+  if user_input == '+':
+    output = a+b
+    print("Additing of the two numbers is ", output)
   elif user_input == '-':
-    print("Subraction of the two numbers is ", a-b)
+    output = a-b
+    print("Subraction of the two numbers is ", output)
   elif user_input == '*':
-    print("Multiplication of the two numbers is ", a*b)
+    output = a*b
+    print("Multiplication of the two numbers is ", output)
   elif user_input == '/':
-    print("First number divided by the second number is ", a/b)
+    output = a/b
+    print("First number divided by the second number is ", output )
   else:
     print("unknown input")
