3afc404c99678db45a6d296ef0ffa08292dc5da7
Sreekar Swarnapuri Pandadu <sreekar.swarnapuri@hotmail.com>
2019-09-23 23:49:27 +0530

    Update FunctionBasics.py

--- Functions/FunctionBasics.py
+++ Functions/FunctionBasics.py
@@ -3,3 +3,15 @@ print("hello")
 print(list(range(2,20)))
 #We type the name of the function before paranthesis and values for the function in the paranthesis
 print(list(range(2,10,3)))
+
+#In addition to using pre-defined functions, we can also create custom funcitons
+#creating a basic function without inputs or outputs
+def func():
+  print("1")
+  print("2")
+  print("3")
+  print("4")
+
+#Functions get executed only when they are called
+func()
+
