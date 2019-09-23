782551ce6aacf96ad6063f14656851afa6ebfd5f
Sreekar Swarnapuri Pandadu <sreekar.swarnapuri@hotmail.com>
2019-09-23 23:45:00 +0530

    Create FunctionBasics.py

---
+++ Functions/FunctionBasics.py
@@ -0,0 +1,5 @@
+#We have already used funcitons which are called pre-defined.
+print("hello")
+print(list(range(2,20)))
+#We type the name of the function before paranthesis and values for the function in the paranthesis
+print(list(range(2,10,3)))
