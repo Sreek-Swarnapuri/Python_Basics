fd3e7c65eff155a57d9d1ed8f8f5390c1629a296
Sreekar Swarnapuri Pandadu <sreekar.swarnapuri@hotmail.com>
2019-09-24 21:26:42 +0530

    Update ReturningFromFunctions.py

--- Functions/ReturningFromFunctions.py
+++ Functions/ReturningFromFunctions.py
@@ -4,4 +4,10 @@ def subraction(a,b):
 
 print(subraction(2,0.3))
 
-#
+#Once the function hits return statment, it will not execute any other statements after it
+def multiply(a,b):
+  mul = a*b
+  return mul
+  print("This will not be printed")
+
+print(multiply(4,7))
