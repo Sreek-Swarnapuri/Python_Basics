278824cc6fef7c4b792379237b9e79d63be6e11f
Sreekar Swarnapuri Pandadu <sreekar.swarnapuri@hotmail.com>
2019-09-25 21:47:20 +0530

    Update FunctionsAsObjects.py

--- Functions/FunctionsAsObjects.py
+++ Functions/FunctionsAsObjects.py
@@ -4,3 +4,12 @@ def powOf(a,b):
 
 p = powOf
 print(p(2,3))
+
+#Functions can also be passed as arguments to other functions.
+def add(a,b):
+  return a+b 
+
+def twice(f,a,b):
+  return f(f(a,b),f(a,b))
+
+print(twice(add,1,2))
