b61991a7981d436e4bbfdbff3fe8df3368029f30
Sreekar Swarnapuri Pandadu <sreekar.swarnapuri@hotmail.com>
2019-09-25 21:15:17 +0530

    Update CommentsAndDocstrings.py

--- CommentsAndDocstrings.py
+++ CommentsAndDocstrings.py
@@ -8,3 +8,15 @@ y = 4
 print(y%x)
 #print(x//y) 
 #As you can see above comment will not be excuted
+#Comments are not retained at runtime as they are ignored.
+
+#Docstrings - Document strings, 
+#Docstrings are similar to comments however, they are designed for explaing total code. For example of a function.
+def add(a,b):
+  """
+  This function addes two numbers and returns the sum. 
+  It takes two inputs and returns an output
+  """
+  return a+b
+
+print(add(1,3))
