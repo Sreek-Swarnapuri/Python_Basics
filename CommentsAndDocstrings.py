8ab9250009827a3b8cf4cfbbe80fd6feb7c03bf5
Sreekar Swarnapuri Pandadu <sreekar.swarnapuri@hotmail.com>
2019-09-25 15:47:22 +0530

    Create CommentsAndDocstrings.py

---
+++ CommentsAndDocstrings.py
@@ -0,0 +1,10 @@
+#Comments are annotions which are used for making the code easier to understand
+#Creating a variable x and assigning it a value of 2
+x = 2
+#Creating a varialbe y and assigning it a value of 4
+y = 4
+
+#Finding and printing the reminder when dividing y by x
+print(y%x)
+#print(x//y) 
+#As you can see above comment will not be excuted
