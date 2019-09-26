0692b4e9ae3a6272fef0b0e1c1a3698e926894b7
Sreekar Swarnapuri Pandadu <sreekar.swarnapuri@hotmail.com>
2019-09-26 22:24:33 +0530

    Create ModuleBasics.py

---
+++ ModuleBasics.py
@@ -0,0 +1,8 @@
+#Modules are codes written by other people for common tasks.
+#Examples are generating random numbers, performing mathematical operations etc
+#We use the keyword import to get the modules that were built already. This line should be at the top of the code so that we can use its features whenever required.
+
+import random
+
+for i in range(5):
+  print(random.randint(1,6))
