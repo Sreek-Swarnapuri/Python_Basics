249897a9d15793ada15cebeb4be653da3264929a
Sreekar Swarnapuri Pandadu <sreekar.swarnapuri@hotmail.com>
2019-09-26 22:26:51 +0530

    Update ModuleBasics.py

--- ModuleBasics.py
+++ ModuleBasics.py
@@ -1,8 +1,13 @@
 #Modules are codes written by other people for common tasks.
 #Examples are generating random numbers, performing mathematical operations etc
-#We use the keyword import to get the modules that were built already. This line should be at the top of the code so that we can use its features whenever required.
+#We use the keyword import to get the modules that were built already. 
+#Import line should be at the top of the code so that we can use its features whenever required.
 
 import random
 
 for i in range(5):
   print(random.randint(1,6))
+
+  #We can import only certain set of functions from modules. We use from 'module_name import var', var being the function
+from math import pi
+print(pi)
