cf6c3229042994d22080e0fb8a899c88098a11ae
Sreekar Swarnapuri Pandadu <sreekar.swarnapuri@hotmail.com>
2019-09-30 23:38:52 +0530

    Update ExceptionHandling.py

--- Exceptions/ExceptionHandling.py
+++ Exceptions/ExceptionHandling.py
@@ -1,1 +1,8 @@
-#To handle exceptions we use 
+#To handle exceptions we use try and except with the error definition
+
+try:
+  num1 = 19
+  num2 = 0
+  print(num1/num2)
+except ZeroDivisionError:
+  print("An error occured: Due to zero division error")
