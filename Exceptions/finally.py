f8d128ab8ec4c010b61b40c338a975ed2381220a
Sreekar Swarnapuri Pandadu <sreekar.swarnapuri@hotmail.com>
2019-10-04 23:56:23 +0530

    Create finally.py

---
+++ Exceptions/finally.py
@@ -0,0 +1,9 @@
+#We can use finally statement which is set after the all the except blocks in order to execute any code which 
+#we would want to run even if there are any exceptions or otherwise
+try:
+   print("Hello")
+   print(1 / 0)
+except ZeroDivisionError:
+   print("Divided by zero")
+finally:
+   print("This code will run no matter what")
