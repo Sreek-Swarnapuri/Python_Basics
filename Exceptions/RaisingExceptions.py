d17a0a69f26861a66e997133143719cf075fd973
Sreekar Swarnapuri Pandadu <sreekar.swarnapuri@hotmail.com>
2019-10-12 23:58:38 +0530

    Update RaisingExceptions.py

--- Exceptions/RaisingExceptions.py
+++ Exceptions/RaisingExceptions.py
@@ -1,5 +1,15 @@
 #We can raise exceptions using the raise keyword
-
 print(1)
 raise ValueError
 print(2)
+
+#Exceptions can be raised with arguments that give detail about them
+name = "123"
+raise NameError("Invalid name")
+
+#We can use raise in Except block to raise exceptions without knowing what exception it is
+try:
+  print(5/0)
+except:
+  print("An Error Occured")
+  raise
