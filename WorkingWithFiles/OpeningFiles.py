fea9d507e4de893016d4c5b041978a71817b7175
Sreekar Swarnapuri Pandadu <sreekar.swarnapuri@hotmail.com>
2019-10-20 16:58:27 +0530

    Update OpeningFiles.py

--- WorkingWithFiles/OpeningFiles.py
+++ WorkingWithFiles/OpeningFiles.py
@@ -1,3 +1,16 @@
 #Text files are easiest to manipulate
 myfile = open("openfile.txt")
 
+#We can specify a mode when opening a file, this would be a second parameter for open function
+# r -- read - reads files, default if no second argument is provided
+# w -- write  - writes to files and rewrites over existing data
+# a -- append - adds data to the end of the file.
+# Adding "b" to the mode opens the file in binary mode for non text files
+
+open("openfile.txt","r")
+
+open("openfile.txt","w")
+
+open("openfile.txt","a")
+
+open("openfile.txt","rb")
