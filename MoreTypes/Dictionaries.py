d2af899f3b13f238c7667c04ea368b65c6921922
Sreekar Swarnapuri Pandadu <sreekar.swarnapuri@hotmail.com>
2019-11-13 11:52:49 +0530

    Create Dictionaries.py

---
+++ MoreTypes/Dictionaries.py
@@ -0,0 +1,17 @@
+"""
+## Dictionaries
+
+Dictionaries are data structures used to map arbitrary keys to values.
+Lists can be thought of as dictionaries with **integer** keys within a certain range.
+Dictionaries can indexed in the same way as lists, using **square brackers** containing keys.
+"""
+
+ages = {"Dave":24,"Mary":42,"Kevin":22}
+print(ages)
+print(ages["Dave"])
+print(ages["Mary"])
+
+# Output:
+# {'Dave': 24, 'Mary': 42, 'Kevin': 22}
+# 24
+# 42
